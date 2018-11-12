66147303b24ff49a009093aba1d33575f078ea07
Miguel Martinez <martinezpmg9@gmail.com>
2018-11-12 19:58:38 -0400

    Movement by tapping
    - Input controller using TouchScript
    - Movement controller scipt
    - Vector2 variable as asset

---
+++ Assets/Scripts/GlobalTap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+
+/// <summary>
+/// Tap controller using TouchScript package
+/// </summary>
+public class GlobalTap : MonoBehaviour
+{
+    private TapGesture tap_g;
+
+    public Vector2Variable touchPosition;
+    public Vector2Variable touchPositionRaw;
+
+
+    private void OnEnable()
+    {
+        tap_g = GetComponent<TapGesture>();
+
+        tap_g.Tapped += tappedHandler;
+    }
+
+    private void OnDisable()
+    {
+        tap_g.Tapped -= tappedHandler;
+    }
+
+    private void tappedHandler(object sender, System.EventArgs e)
+    {
+        touchPositionRaw.SetValue(tap_g.ScreenPosition);
+        touchPosition.SetValue(Camera.main.ScreenToWorldPoint(tap_g.ScreenPosition));        
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Move to point controller.
+/// 
+/// Inpired by https://www.youtube.com/watch?v=Cw6B-VYNba0
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 velDir;
+    [Range(0f, 10f)]
+    public float velocityMagnitude = 1f;
+
+    public Vector2Variable targetPos;    
+    private float targetDistance = 0f;
+    [Range(0f, 1f)]
+    public float targetTreshold = 1f;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        targetDistance = Vector2.Distance(transform.position, targetPos.Value);
+    }
+
+    private void FixedUpdate()
+    {
+        if (targetTreshold > targetDistance)
+            velDir = Vector2.zero;
+        else
+            velDir = (targetPos.Value - (Vector2)transform.position).normalized;
+
+        rb.velocity = velDir * velocityMagnitude;   
+    }
+}
